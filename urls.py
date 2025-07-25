3315307609c6b9b20b6fadcd331c714c810729ed
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-07-25 21:35:52 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 
+#urls
+
 urlpatterns = [
     path('users/list/', UsersListApiView.as_view()),
     path('reports/list/', ReportsListApiView.as_view()),
